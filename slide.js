e5c6018b837556d3698b66101f01e86ed75fe90d
Osuoha Chidokwa Nancy <39951626+OsuohaNancy2020@users.noreply.github.com>
2022-06-25 11:46:28 +0800

    Add files via upload

---
+++ slide.js
@@ -0,0 +1,13 @@
+//alert('Osuoha Chidokwa Nancy');
+
+//let myname, height, country
+
+// myname = Chidokwa;
+// height = 6;
+// country = Nigeria;
+
+alert("Osuoha Chidokwa, Height: 6ft, Country: Nigeria")
+//alert.getElementById("height").innerHTML = height;
+//document.getElementById("country").innerHTML = country;
+
+//print(myname);
